46047a1620dffef863d6635f274edf11b40a257b
navneet <knavneet@qualminds.com>
2022-04-18 16:49:17 +0530

    add required,return type typescript

--- HTML/utility.js
+++ HTML/utility.js
@@ -3,9 +3,8 @@
 //   x: number;
 //   y: number;
 // }
-var pointPart = {
-    x: 10,
-    y: 10,
-    z: 10
-}; // `Partial` allows x and y to be optional
-console.log(pointPart);
+var point = {
+    x: '10',
+    y: 20
+};
+console.log(typeof point);
